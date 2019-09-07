cc525d882620e428ef8b6adf6fff99a0da4fafb2
심현용 <jonesn5508@gmail.com>
2019-09-07 23:51:07 +0900

    Imprementation widget

--- app/src/main/java/com/nanodegree/hyunyong/microdotstatus/AQIWidget.java
+++ app/src/main/java/com/nanodegree/hyunyong/microdotstatus/AQIWidget.java
@@ -3,28 +3,70 @@ package com.nanodegree.hyunyong.microdotstatus;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.widget.RemoteViews;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.room.Room;
+
+import com.nanodegree.hyunyong.microdotstatus.data.City;
+import com.nanodegree.hyunyong.microdotstatus.data.Microdot;
+import com.nanodegree.hyunyong.microdotstatus.data.Repository;
+import com.nanodegree.hyunyong.microdotstatus.data.ResponseState;
+import com.nanodegree.hyunyong.microdotstatus.data.Webservice;
+import com.nanodegree.hyunyong.microdotstatus.db.AppDatabase;
+import com.nanodegree.hyunyong.microdotstatus.db.CityDao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class AQIWidget extends AppWidgetProvider {
+    static void updateAppWidget(Context context, final AppWidgetManager appWidgetManager,
+                                final int appWidgetId) {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
 
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.aqiwidget);
+        final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.aqiwidget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
 
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        // There may be multiple widgets active, so update all of them
+        CityDao dao = Room.databaseBuilder(context, AppDatabase.class, "app-db").allowMainThreadQueries().build().cityDao();
+        Repository repository = new Repository(new Retrofit.Builder()
+                .baseUrl("https://api.waqi.info/")
+                .client(new OkHttpClient())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build().create(Webservice.class));
+        if (dao == null) return;
+        List<City> cityList = dao.getCities();
+        for (int i = 0; i < cityList.size() ; i++) {
+            final City city = cityList.get(i);
+            repository.getFeedFromLocation(city.getGeo().get(0), city.getGeo().get(1)).observeForever(new Observer<ResponseState>() {
+                @Override
+                public void onChanged(ResponseState responseState) {
+                    if ("ok".equals(responseState.getStatus())) {
+                        Microdot microdot = responseState.getData();
+                        views.setTextViewText(R.id.city_name, Utils.getSimpleCityName(city.getName()));
+                        views.setImageViewResource(R.id.iv_icon, Utils.getDrawableResourceByAqi(microdot.getAqi()));
+                        views.setTextViewText(R.id.tv_marker, String.valueOf(microdot.getAqi()));
+                        views.setInt(R.id.bg_conatainer, "setBackgroundResource", Utils.getColorResourceByAqi(microdot.getAqi()));
+                        appWidgetManager.updateAppWidget(appWidgetId, views);
+                    }
+                }
+            });
+        }
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
@@ -32,7 +74,7 @@ public class AQIWidget extends AppWidgetProvider {
 
     @Override
     public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
+
     }
 
     @Override
--- app/src/main/java/com/nanodegree/hyunyong/microdotstatus/data/Repository.java
+++ app/src/main/java/com/nanodegree/hyunyong/microdotstatus/data/Repository.java
@@ -5,9 +5,6 @@ import android.util.Log;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
-import com.nanodegree.hyunyong.microdotstatus.db.AppDatabase;
-import com.nanodegree.hyunyong.microdotstatus.db.CityDao;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
